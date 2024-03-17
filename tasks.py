3cbd93f7429ea55b2fa548d1d83dfbdbaed11349
Zoom.Quiet <zoom.quiet@gmail.com>
2024-03-17 09:10:03 +0800

    📖 DOC:

--- tasks.py
+++ tasks.py
@@ -2,22 +2,22 @@ import functools
 import os
 
 from invoke import task
-#from fabric.api import env, lcd, local, task
+
+# from fabric.api import env, lcd, local, task
 
 # Local path configuration (can be absolute or relative to fabfile)
-env = {"input_path" : 'content'
-    , "deploy_path" : 'output'
-    }
+env = {"input_path": "content", "deploy_path": "output"}
 
 
 @task
 def build(c):
-    c.run('pelican {input_path} -o {deploy_path} -s pelicanconf.py'.format(**env))
+    c.run("pelican {input_path} -o {deploy_path} -s pelicanconf.py".format(**env))
+    # c.run('pelican {input_path} -v -o {deploy_path} -s pelicanconf.py'.format(**env))
 
 
 def serve(c):
-    #c.run('cd {deploy_path} && python -m SimpleHTTPServer'.format(**env))
-    c.run('cd {deploy_path} && python3 -m http.server'.format(**env))
+    # c.run('cd {deploy_path} && python -m SimpleHTTPServer'.format(**env))
+    c.run("cd {deploy_path} && python3 -m http.server".format(**env))
 
 
 @task
@@ -27,15 +27,17 @@ def reserve(c):
 
 
 def gh_pages(c):
-    c.run('cd {deploy_path} && '
-        'pwd && '
-        'git st && '
-        'git add --all . && '
+    c.run(
+        "cd {deploy_path} && "
+        "pwd && "
+        "git st && "
+        "git add --all . && "
         'git ci -am "re-build from local by markdoc @MBP111216ZQ" && '
         # 'git pu cafe gitcafe-page '
-        'git pu && '
-        'date '.format(**env)
-        )
+        "git pu && "
+        "date ".format(**env)
+    )
+
 
 @task
 def pub(c):
@@ -46,14 +48,10 @@ def pub(c):
 
 
 def pull_data(c):
-    c.run(
-        'cd {deploy_path} && '
-        'pwd && '
-        'git pull'.format(**env)
-    )
+    c.run("cd {deploy_path} && " "pwd && " "git pull".format(**env))
 
 
-'''
+"""
 env.deploy_7niu = '7niu'
 env.qiniu_bin = '/opt/bin/7niu_package_darwin_amd64/qrsync'
 env.qiniu_conf = '../7niu4pychina.json'
@@ -123,4 +121,4 @@ def install_deps():
         'Markdown '
         'pelican'
     )
-'''
+"""
